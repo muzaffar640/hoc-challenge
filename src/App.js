189ea42778ec488f54ffa38d1803e7b8992a164a
muzaffar-tgt <muzaffar.h@thoughtgreen.com>
2022-02-22 22:48:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const ColorButton = (props) => (
+    <div style={{ backgroundColor: props.color }}>
+      <Counter />
+    </div>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Challenge</h1>
+      <h2>
+        Change the counter background color only using hoc, without touching
+        Counter component
+      </h2>
+      <ColorButton color="#ff4d4d" />
+      <ColorButton color="#196619" />
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h3>{count}</h3>
+
+      <button onClick={() => setCount(count + 1)}>Update</button>
     </div>
   );
 }
